473f9393102821b797f8771c009149e3ad75cfcd
SA\YZ223CY <maximiliano.e.pozzi@gds.ey.com>
2022-01-28 19:20:48 -0300

    colyseus social + onAuth

--- src/MyRoom.ts
+++ src/MyRoom.ts
@@ -1,9 +1,24 @@
 // MyRoom.ts
-import { Room } from "@colyseus/core";
+import { Room, Client } from "colyseus";
 import { MyState } from "./MyState";
+import http from "http";
 
 export class MyRoom extends Room<MyState> {
     onCreate() {
         this.setState(new MyState());
     }
+    // Authorize client based on provided options before WebSocket handshake is complete
+    onAuth (client: Client, options: any, request: http.IncomingMessage) {
+        console.log(options.accessToken);
+        return true;
+    }
+
+    // When client successfully join the room
+    onJoin (client: Client, options: any, auth: any) { }
+
+    // When a client leaves the room
+    onLeave (client: Client, consented: boolean) { }
+
+    // Cleanup callback, called after there are no more clients in the room. (see `autoDispose`)
+    onDispose () { }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // Colyseus + Express
 import { Server } from "@colyseus/core";
 import { uWebSocketsTransport } from "@colyseus/uwebsockets-transport";
+import { MyRoom } from "./MyRoom";
+
 const port = Number(process.env.port) || 3000;
 
 const gameServer = new Server({
@@ -10,3 +12,11 @@ const gameServer = new Server({
 });
 
 gameServer.listen(port);
+
+
+// Define "chat" room
+gameServer.define("myRoom", MyRoom)
+  .on("create", (room) => console.log("room created:", room.roomId))
+  .on("dispose", (room) => console.log("room disposed:", room.roomId))
+  .on("join", (room, client) => console.log(client.id, "joined", room.roomId))
+  .on("leave", (room, client) => console.log(client.id, "left", room.roomId));;
