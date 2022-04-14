750ac1b6a831ca6c80e7bc7a180525713ebbcdd6
Johan Maximus <maxi4220@hotmail.com>
2022-04-14 02:15:03 -0300

    Added try catch to the index

--- src/index.ts
+++ src/index.ts
@@ -9,34 +9,40 @@ import { WebSocketTransport } from "@colyseus/ws-transport"
 import { MyRoom } from "./MyRoom";
 import { EventHandler } from "./handlers/event.handler";
 
-const app = express();
-const server = createServer(app); // create the http server manually
+try{
+  const app = express();
+  const server = createServer(app); // create the http server manually
 
-app.use(cors({
-  origin: "*",
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}))
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+  app.use(cors({
+    origin: "*",
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }))
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
-const port = Number(process.env.port) || 3000;
+  const port = Number(process.env.port) || 3000;
 
-const gameServer = new Server({
-  transport: new WebSocketTransport({
-      server // provide the custom server for `WebSocketTransport`
-  })
-});
+  const gameServer = new Server({
+    transport: new WebSocketTransport({
+        server // provide the custom server for `WebSocketTransport`
+    })
+  });
 
-gameServer.listen(port);
+  gameServer.listen(port);
 
-const eventHandler = new EventHandler();
-eventHandler.setupEvents(app);
+  const eventHandler = new EventHandler();
+  eventHandler.setupEvents(app);
 
-// Define "chat" room
-gameServer.define("myRoom", MyRoom)
-  .on("create", (room) => console.log("room created:", room.roomId))
-  .on("dispose", (room) => console.log("room disposed:", room.roomId))
-  .on("join", (room, client) => console.log(client.id, "joined", room.roomId))
-  .on("leave", (room, client) => console.log(client.id, "left", room.roomId));
+  // Define "chat" room
+  gameServer.define("myRoom", MyRoom)
+    .on("create", (room) => console.log("room created:", room.roomId))
+    .on("dispose", (room) => console.log("room disposed:", room.roomId))
+    .on("join", (room, client) => console.log(client.id, "joined", room.roomId))
+    .on("leave", (room, client) => console.log(client.id, "left", room.roomId));
 
-console.log("Server started at port: "+port.toString())
+  console.log("Server started at port: "+port.toString())
+}
+catch(err) {
+  console.log("Error")
+  console.error(err);
+}
